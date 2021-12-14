ce0ae1e60c27114244caab60be8e0c1464d284df
Daffa6633 <Daffa6633@gmail.com>
2021-12-14 17:51:51 +0700

    Initials V1.1

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <title>Lare-Baguette</title>
+</head>
+
+<body>
+    <!-- React root DOM -->
+    <div id="user">
+    </div>
+
+    <!-- React JS -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/manifest.js') }}" defer></script>
+    <script src="{{ asset('js/vendor.js') }}" defer></script>
+</body>
+
+</html>
